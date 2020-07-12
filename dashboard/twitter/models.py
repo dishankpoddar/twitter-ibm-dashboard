b5b9fff5fc8c2b50e5c2d422d78f22c7d7019281
Dishank <dishankpoddar@gmail.com>
2020-07-12 16:19:31 +0530

    kuch to kaam

--- dashboard/twitter/models.py
+++ dashboard/twitter/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class CustomBooleanField(models.IntegerField):
+    def from_db_value(self, value, expression, connection, context):
+        if value is None:
+            return value
+        return True if value else False
+
+class Tweets(models.Model):
+    index = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    username = models.TextField(max_length=255)
+    name = models.TextField(max_length=255)
+    tweet = models.TextField(max_length=280)
+    geo = models.CharField(max_length=25)
+    positive = models.BooleanField(default=False)
+    happy = models.BooleanField(default=False)
+    relief = models.BooleanField(default=False)
+    neutral = models.BooleanField(default=False)
+    anxious = models.BooleanField(default=False)
+    sad = models.BooleanField(default=False)
+    negative = models.BooleanField(default=False)
--- dashboard/twitter/views.py
+++ dashboard/twitter/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse, redirect,get_object_or_404
+from django.views.generic import (View,TemplateView,CreateView, DeleteView, DetailView, 
+                                    ListView,UpdateView,FormView)
+from django.db import transaction
+from django.db.models import Avg, Count, F, Aggregate
+from django.conf import settings 
 
-# Create your views here.
+def index(request):
+    context = {
+        'india_map': settings.MEDIA_ROOT+"/india.svg"
+    }
+    return render(request, 'twitter/index.html',context)
