15f6ea995230250bd973fdaf5357472f94bfaebc
Dishank <dishankpoddar@gmail.com>
2020-07-14 02:37:57 +0530

    bar query processed

--- dashboard/twitter/views.py
+++ dashboard/twitter/views.py
@@ -9,7 +9,9 @@ from .models import Tweets
 from functools import reduce
 import operator
 
-
+import time
+from datetime import date,timedelta
+from datetime import datetime
 
 from wordcloud import WordCloud, STOPWORDS 
 import matplotlib.pyplot as plt
@@ -29,13 +31,17 @@ def word_count(my_string,stopwords):
     return counts
 
 def bar(request):
+    start = time.time()
     all_tweets = Tweets.objects.all()
+    filter_date = date.today()
     if(request.method == 'POST'):
         print('yay')
     tweet_cloud = ""
     for tweet in all_tweets:
         tweet_cloud += " "+tweet.tweet
     stopwords = STOPWORDS|set(['covid','corona','lockdown','quarantine'])|set(["<NATURE>","<FOOD>","<SPACE-TIME>","<ACTIVITIES>","<OBJECTS>","<SYMBOLS>","<FLAGS>","<PERSON>","<HAPPY>","<LAUGH>","<LOVE>","<SARCASM>","<DOUBT>","<UNWELL>","<SAD>","<PISSED>","<SLEEPY>","<NEUTRAL>","<SHOCK>","<PATRIOT>"]) 
+    
+    
     wordcloud = WordCloud(width = 720, height = 360, 
                 background_color ='#010232', 
                 stopwords = stopwords, 
@@ -46,21 +52,61 @@ def bar(request):
     plt.tight_layout(pad = 0) 
     plt.savefig('./twitter/static/twitter/word.png')
 
-    pairs = word_count(tweet_cloud,stopwords)[:5]
-    top_5 = [pair[0] for pair in pairs]
-    top_5_tweets = Tweets.objects.filter(reduce(operator.and_, (Q(tweet__contains=x) for x in top_5)))
-    print(top_5.count)
-    neg_top_5 = top_5_tweets.filter(Q(negative=True)|Q(sad=True)|Q(anxious=True)).count()
-    pos_top_5 = top_5_tweets.filter(Q(positive=True)|Q(happy=True)|Q(relief=True)).count()
-    print(neg_top_5,pos_top_5)
+    top_5 = word_count(tweet_cloud,stopwords)[:5]
+    top_5 = [[top[0]] for top in top_5]
+    for top in top_5:
+        top_tweet = Tweets.objects.filter(tweet__contains=top[0])
+        neg_top_5 = top_tweet.filter(Q(negative=True)|Q(sad=True)|Q(anxious=True)).count()
+        pos_top_5 = top_tweet.filter(Q(positive=True)|Q(happy=True)|Q(relief=True)).count()
+        top.extend([neg_top_5,pos_top_5])
+        print(top)
     
+    filter_date = datetime.strptime("2020-06-12", "%Y-%m-%d").date()
+    #print(Tweets.objects.filter(date=filter_date))
+
+    pos_neg_filter_date = filter_date
+    pos_neg_days = []
+    for i in range(7):
+        pos_neg_days.append([pos_neg_filter_date])
+        date_tweet = Tweets.objects.filter(date=pos_neg_filter_date)
+        neg_date_tweet = date_tweet.filter(Q(negative=True)|Q(sad=True)|Q(anxious=True)).count()
+        pos_date_tweet = date_tweet.filter(Q(positive=True)|Q(happy=True)|Q(relief=True)).count()
+        try:
+            neg_perc = neg_date_tweet/(neg_date_tweet+pos_date_tweet)
+            pos_perc = pos_date_tweet/(neg_date_tweet+pos_date_tweet)
+        except:
+            neg_perc = 0
+            pos_perc = 0
+        pos_neg_days[i].extend([neg_perc,pos_perc])
+        pos_neg_filter_date -= timedelta(days=1)
+        print(pos_neg_days[i])
+
+    emotion_filter_date = filter_date
+    emotion_days = []
+    for i in range(7):
+        emotion_days.append([emotion_filter_date])
+        date_tweet = Tweets.objects.filter(date=emotion_filter_date)
+        neg_date_tweet = date_tweet.filter(Q(negative=True)).count()
+        sad_date_tweet = date_tweet.filter(Q(sad=True)).count()
+        anx_date_tweet = date_tweet.filter(Q(anxious=True)).count()
+        neu_date_tweet = date_tweet.filter(Q(neutral=True)).count()
+        rel_date_tweet = date_tweet.filter(Q(relief=True)).count()
+        hap_date_tweet = date_tweet.filter(Q(happy=True)).count()
+        pos_date_tweet = date_tweet.filter(Q(positive=True)).count()
+        emotion_days[i].extend([neg_date_tweet,sad_date_tweet,anx_date_tweet,neu_date_tweet,rel_date_tweet,hap_date_tweet,pos_date_tweet])
+        emotion_filter_date -= timedelta(days=1)
+        print(emotion_days[i])
+    end = time.time()
+	
+
 
 
     context = {
         'bar' : True,
         'wordcloud' :settings.MEDIA_ROOT+"/word.png"
     }
-    return render(request, 'twitter/bar.html',context)
+    return HttpResponse(f"Runtime of the query is {end - start}")
+    #return render(request, 'twitter/bar.html',context)
 
 def line(request):
     context = {
